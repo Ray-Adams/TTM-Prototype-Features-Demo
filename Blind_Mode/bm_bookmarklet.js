885412bff7ada983535af64f1ad91e18ed5c8c62
Ray Adams <63924749+Ray-Adams@users.noreply.github.com>
2020-07-24 11:56:45 -0700

    Create bm_bookmarklet.js

---
+++ Blind_Mode/bm_bookmarklet.js
@@ -0,0 +1,1 @@
+javascript:(()=>{var a=document.styleSheets[16],b="{color:#e2b714; transition:0.3s;}";a.deleteRule(10),a.deleteRule(9),a.insertRule(".correct"+b,9),a.insertRule(".incorrect"+b,10)})()
