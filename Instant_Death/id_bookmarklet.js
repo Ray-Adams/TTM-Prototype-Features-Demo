77b33b8da3b12912d6931246ab3de4e0d3264fca
Ray Adams <63924749+Ray-Adams@users.noreply.github.com>
2020-07-24 13:55:24 -0700

    Create id_bookmarklet.js

---
+++ Instant_Death/id_bookmarklet.js
@@ -0,0 +1,1 @@
+javascript:window.addEventListener("keyup",()=>{var e=document.getElementsByClassName("char current incorrect")[0];null!=e&&(alert(`Correct key: ${e.innerHTML}`),window.location.reload())});
